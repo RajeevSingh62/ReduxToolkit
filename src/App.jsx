9fee6a2dc72ee99bc230c0a5d5291461ffd3ed79
Rajeev Singh <Rajeevsingh629944@gmail.com>
2025-04-09 00:19:45 +0530

    add to cart functionality and increase quantity

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   {/* <Header/> */}
   <ProductsList/> 
   {/* <AddProducts/>  */}
-  {/* <Cart/> */}
+  <Cart/>
 
   </>
   )
--- src/redux/CartSlice.js
+++ src/redux/CartSlice.js
@@ -11,12 +11,22 @@ const cartSlice = createSlice({
   initialState: initialState,
   reducers: {
     addToCart: (state, action) => {
-      alert("added to cart");
+      alert("add to cart");
+      const item = action.payload;
+      const existingItem = state.cartItems.find((p) => p.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.cartItems.push({ ...item, quantity: 1 });
+      }
     },
     removeFromCart: () => {},
     clearCart: () => {},
     decreaseQuantity: () => {},
-    increaseQuantity: () => {},
+    increaseQuantity: (state, action) => {
+      const item = state.cartItems.find((p) => p.id === action.payload);
+      if (item) item.quantity += 1;
+    },
   },
 });
 export const {
